7db7e822849bcbcd3804ec74f6a1a51d4e55086c
Fernando A. Guerra B. <fguerrab@MacBook-Pro-de-Fernando.local>
2026-05-12 16:23:40 -0400

    fix: add pg to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["ssh2", "tunnel-ssh", "mysql2"],
+  serverExternalPackages: ["ssh2", "tunnel-ssh", "mysql2", "pg"],
 };
 
 export default nextConfig;
